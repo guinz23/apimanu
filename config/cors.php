16a4d6bda3ada1bf81ee6e72128cf12ee1a4a8c8
Juan Gabriel <juangabriellopez30@gmail.com>
2021-02-13 00:55:41 -0600

    add soluction for schema error

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['https://apimanu.herokuapp.com/'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.apimanu.herokuapp.com'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
